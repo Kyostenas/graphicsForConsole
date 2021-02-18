e1b1c901a9e4ae8a194b6027f8c010a6d754e63d
Benjamín Ramírez <chilerito12@gmail.com>
2021-02-18 13:50:39 -0600

    added required files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pretty-graphics-console',         
+  packages = ['pretty-graphics-console'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'All sorts of tables and graphics for console (for now just tables) ',   
+  author = 'Benjamin Ramirez',                   
+  author_email = 'chilerito12@gmail.com',      
+  url = 'https://github.com/Kyostenas/pretty-graphics-console',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  install_requires=[            
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
